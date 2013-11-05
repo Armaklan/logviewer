17f1d8242711c861cd71d262eacf4953cd22dcc8
Armaklan <lionel.zuber@armaklan.org>
2013-11-05 21:06:21 +0100

    Correction bowerrc Ajout de la glue sur la zone Gestion de log multiple Reste problème => En cas de disconnect & reconnect, il me redonne pas les anciennes lignes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,49 +2,98 @@
  * Created by bennekroufm on 19/10/13.
  */
 
-var pocazApp = angular.module('pocaz', []);
+var pocazApp = angular.module('pocaz', ['luegg.directives']);
 
-pocazApp.factory('socket', ['$rootScope', function ($rootScope) {
-
-    var socket = io.connect();
-
-    return {
-      on: function (eventName, callback) {
-        socket.on(eventName, function () {  
-          var args = arguments;
-          $rootScope.$apply(function () {
-            callback.apply(socket, args);
-          });
-        });
-      },
-      emit: function (eventName, data, callback) {
-        socket.emit(eventName, data, function () {
-          var args = arguments;
-          $rootScope.$apply(function () {
-            if (callback) {
-              callback.apply(socket, args);
-            }
-          });
-        })
-      },
-      disconnect: function () {
-        socket.disconnect();
-      },
-      socket: socket
-    };
-
-}]);
+pocazApp.factory('socket', function ($rootScope) {
+  return {
+    giveSocket: function() {
+        var disconnecting = false;
+        var socket = {};
+        return {
+          connect: function(logId){
+            disconnecting = false;
+            socket = io.connect("/" + logId);
+          },
+          on: function(eventName, callback){
+            socket.on(eventName, function(){
+              var args = arguments;
+              if(!disconnecting){
+                $rootScope.$apply(function(){
+                  callback.apply(socket, args);
+                });
+              }
+              else {
+                callback.apply(socket, args);
+              }
+            });
+          },
+          emit: function (eventName, data, callback) {
+            socket.emit(eventName, data, function(){
+              var args = arguments;
+              $rootScope.$apply(function(){
+                if (callback) {
+                  callback.apply(socket, args);
+                }
+              });
+            })
+          },
+          disconnect: function(){
+            disconnecting = true;
+            socket.disconnect();
+          },
+          socket: socket
+        };
+    }
+  }
+});
 
 
 pocazApp.controller('pocazCtrl', function(socket, $scope){
 
     $scope.msg = [];
+    $scope.selectedLog = 1;
+    $scope.glued = true;
 
-    socket.on('Log', function(result) {
+    $scope.logs = [];
+    $scope.logs[0] = {
+      id: 1,
+      css: 'active'
+    };
+
+    $scope.logs[1] = {
+      id: 2,
+      css: ''
+    };
+
+    $scope.sock = socket.giveSocket();
+    $scope.sock.connect($scope.selectedLog);
+    $scope.sock.on('Log', function(result) {
         $scope.msg.push(result);
         if($scope.msg.length > 100) {
             $scope.msg.shift();
         }
     });
+
+    $scope.changeLog = function(log) {
+      $scope.sock.disconnect();
+
+      $scope.msg = [];
+      $scope.logs.forEach(function(log) {
+        log.css = '';
+      });
+      $scope.selectedLog = log.id;
+      log.css = 'active';
+      
+      $scope.sock.disconnect();
+      $scope.sock = socket.giveSocket();
+      $scope.sock.connect($scope.selectedLog);
+      $scope.sock.on('Log', function(result) {
+          $scope.msg.push(result);
+          if($scope.msg.length > 100) {
+              $scope.msg.shift();
+          }
+      });
+
+    }
 
 });
--- server.js
+++ server.js
@@ -8,23 +8,67 @@ var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 
+var logfiles = []
+logfiles[0] = {
+	name: "fluxpousse",
+	url : "c:\\developpement\\test.log"
+};
+logfiles[1] = {
+	name: "fluxpousse",
+	url : "c:\\developpement\\err.log"
+};
 
 Tail = require('tail').Tail;
 
-tail = new Tail("/usr/local/jboss-5.1.0.GA/server/fluxpousse/log/server.log");
+var tail = []
+tail[0] = new Tail(logfiles[0].url);
+tail[1] = new Tail(logfiles[1].url);
 
 app.configure(function() {
     app.use(express.static(__dirname + '/app'));
     app.use(express.errorHandler({ dumpExceptions: true, showStack: false }));
 });
 
+var fs = require('fs'); // file system module
+
 server.listen(7000);
 
-io.sockets.on('connection', function(client){
+io.of('/1').on('connection', function(client){
 
-    tail.on("line", function(data) {
+	fs.readFile(logfiles[0].url, 'utf-8', function(err, data) {
+	    if (err) throw err;
+
+	    var lines = data.trim().split('\n');
+	    var lastLines = lines.slice(-100);
+
+	    lastLines.forEach( function(line) {
+	    	client.emit('Log', line);
+	    });
+	});
+
+
+    tail[0].on("line", function(data) {
         client.emit('Log', data);
     });
 
 });
 
+io.of('/2').on('connection', function(client){
+
+	fs.readFile(logfiles[1].url, 'utf-8', function(err, data) {
+	    if (err) throw err;
+
+	    var lines = data.trim().split('\n');
+	    var lastLines = lines.slice(-100);
+
+	    lastLines.forEach( function(line) {
+	    	client.emit('Log', line);
+	    });
+	});
+
+
+    tail[1].on("line", function(data) {
+        client.emit('Log', data);
+    });
+
+});
